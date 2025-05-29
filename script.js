0ec22b304d871da021b847066cdfa059ce5672bc
saibat01 <mohammedmehdisaibat@gmail.com>
2025-05-29 17:26:14 +0100

    Add falling enemies with random delay, auto-remove after fall, and improve UX by blurring play button

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ const gameContainer = document.querySelector("#game-container");
 const player = document.querySelector("#player");
 const play = document.querySelector("#play-btn");
 
-const moveStep = 20;
+const moveStep = 40;
 const gameContainerWidth = gameContainer.offsetWidth;
 const playerWidth = player.offsetWidth;
 
 let playerLeft = gameContainerWidth / 2 - playerWidth / 2; // start centered
 player.style.left = `${playerLeft}px`;
-
-play.addEventListener("click", () => {
-  player.focus();
-});
 
 player.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
@@ -31,4 +27,40 @@ player.addEventListener("keydown", (e) => {
       player.style.left = `${playerLeft}px`;
     }
   }
+});
+
+//ENEMIES ANIMATION:
+
+function spawnEnemy() {
+  const div = document.createElement("div");
+  div.classList.add("enemy");
+  gameContainer.append(div);
+
+  const enemyWidth = 40;
+  const randomFall = Math.floor(
+    Math.random() * (gameContainerWidth - enemyWidth)
+  );
+
+  div.style.left = `${randomFall}px`;
+
+  const enemyFallDelay = Math.random() * 2;
+
+  div.style.animationDelay = `${enemyFallDelay}s`;
+
+  div.addEventListener("animationend", () => {
+    div.remove();
+  });
+}
+
+let isGameRunning = false;
+let enemySpawner;
+
+play.addEventListener("click", () => {
+  player.focus();
+  play.blur();
+  if (!isGameRunning) {
+    enemySpawner = setInterval(spawnEnemy, 1000);
+    isGameRunning = true;
+  }
+  play.textContent = "Playing";
 });
